5ad2616dec75de00af85d771949d7c5296bed13f
SirGoldMeid <Goldmeidlrt@gmail.com>
2024-08-02 21:30:32 -0400

    caun muchas particulas
    xd

--- client/components/SnowfallEffect.js
+++ client/components/SnowfallEffect.js
@@ -5,8 +5,8 @@ function createSnowflake() {
     snowflake.className = "snowflake";
     snowflake.textContent = "🚀"; // Puedes cambiar esto a cualquier símbolo o carácter
     snowflake.style.left = Math.random() * 100 + "vw";
-    snowflake.style.animationDuration = Math.random() * 5 + 5 + "s"; // Duración aleatoria entre 5s y 10s
-    snowflake.style.fontSize = Math.random() * 20 + 10 + "px"; // Tamaño aleatorio entre 10px y 30px
+    snowflake.style.animationDuration = Math.random() * 8 + 16 + "s"; // Duración aleatoria entre 5s y 10s
+    snowflake.style.fontSize = Math.random() * 15 + 5 + "px"; // Tamaño aleatorio entre 10px y 30px
     snowflake.style.setProperty("--x-pos", Math.random() - 0.5);
 
     document.body.appendChild(snowflake);
